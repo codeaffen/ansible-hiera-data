9e7f8f0124bf48d0ad9bf94915931eb9ab18db9b
Christian Meißner <cme+github@meissner.sh>
2020-12-19 22:03:37 +0100

    Add docstrings and make parameter more readable
    We add inline docstring documentation.
    We also changed the names of parameters for the given hashes, as we
    found it a bit more readable.

--- plugins/module_utils/vars.py
+++ plugins/module_utils/vars.py
@@ -7,16 +7,30 @@ __metaclass__ = type
 from ansible.utils.vars import merge_hash, _validate_mutable_mappings
 
 
-def combine_vars(a, b, hash_behavior='merge', list_behavior='replace'):
-    """
-    Return a copy of dictionaries of variables based on configured hash behavior
-    """
+def combine_vars(left, right, hash_behavior='merge', list_behavior='replace'):
+    """Return a copy of dictionaries of variables based on configured hash behavior.
 
+    This function takes two dictionaries and merge them according to the choosen `hash_behavior`.
+    If there are elements from type `list` they were be handled according to the choosen `list_behavior`.
+    All other types will replace in `left` dict by the values of `right` dict.
+    The work is always done recursively.
+
+    :param left: Dict with lower precedence.
+    :type left: dict
+    :param right: Dict with higher precedence.
+    :type right: dict
+    :param hash_behavior: How should dicts be handled, options are 'merge', 'replace', defaults to 'merge'
+    :type hash_behavior: str, optional
+    :param list_behavior: How should lists be handled, option are 'append', 'append_rp', 'keep', 'prepend', 'prepend_rp', 'replace', defaults to 'replace'
+    :type list_behavior: str, optional
+    :return: Returns a copy of `left` merged with the keys and values from `right`.
+    :rtype: dict
+    """
     if hash_behavior == "merge":
-        return merge_hash(a, b, recursive=True, list_merge=list_behavior)
+        return merge_hash(left, right, recursive=True, list_merge=list_behavior)
     else:
         # HASH_BEHAVIOUR == 'replace'
-        _validate_mutable_mappings(a, b)
-        result = a.copy()
-        result.update(b)
+        _validate_mutable_mappings(left, right)
+        result = left.copy()
+        result.update(right)
         return result
